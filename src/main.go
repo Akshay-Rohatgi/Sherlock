62d5d8f4fc834656bd1301b4a45ad28d5810cd44
Akshay-Rohatgi <52616034+Akshay-Rohatgi@users.noreply.github.com>
2020-08-09 01:04:57 -0700

    figured out args

--- src/main.go
+++ src/main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	// "flag"
 	"fmt"
 	"os"
 )
 
 func main() {
+
+	// get args
+	args := os.Args
+	nicerArgs := args[1:]
+	fmt.Println(nicerArgs)
+
+	// grab hostname lol
+	hostname, err := os.Hostname()
+	if err != nil {
+		hostname = "Could not be found"
+	}
+
 	fmt.Println(`
   ██████  ██░ ██ ▓█████  ██▀███   ██▓     ▒█████   ▄████▄   ██ ▄█▀
 ▒██    ▒ ▓██░ ██▒▓█   ▀ ▓██ ▒ ██▒▓██▒    ▒██▒  ██▒▒██▀ ▀█   ██▄█▒ 
@@ -18,22 +31,9 @@ func main() {
       ░   ░  ░  ░   ░  ░   ░         ░  ░    ░ ░  ░ ░      ░  ░   
                                                   ░               
 A tool built for blue teams and incident response teams
+sherlock@` + hostname + ` is ready
 													  `)
 
-	hostname, err := os.Hostname()
-	if err != nil {
-		hostname = "Could not be found"
-	}
+	
 
-	// addr, err := net.LookupIP(hostname)
-	// if err != nil {
-	// 	addr = nil
-	// }
-
-	fmt.Println(`
-=====================
-Sherlock Initialized
-=====================
-Hostname: ` + hostname + `
-`)
 }
