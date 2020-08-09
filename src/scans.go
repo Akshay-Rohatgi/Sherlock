74f33d55f63f65196f48d005af4edd3835c683df
Akshay-Rohatgi <52616034+Akshay-Rohatgi@users.noreply.github.com>
2020-08-09 13:12:08 -0700

    firewall scan ig

--- src/scans.go
+++ src/scans.go
@@ -2,27 +2,41 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func systemScan(filePath string) {
+	time := time.Now()
+	reportPath := filePath + "/sherlock-scan-report-" + time.Format("01-02-2006")
 
 	fmt.Println("Starting system scan now!")
-	fmt.Println("Saving all files to " + filePath)
+
+	runCommand("mkdir " + reportPath)
+	fmt.Println("Saving all files to " + reportPath)
 
 	// Get iptable rules and write to file
-	runCommand("touch " + filePath + "IPv4IptablesRules.txt")
-	runCommand("iptables -S > " + filePath + "IPv4IptablesRules.txt")
+	runCommand("touch " + reportPath + "IPv4IptablesRules.txt")
+	runCommand("chmod 777 " + reportPath + "IPv4IptablesRules.txt")
+	runCommand("iptables -S > " + reportPath + "IPv4IptablesRules.txt")
 
 
 
 }
 
 func firewallScan(filePath, ipVersion string) {
+	time := time.Now()
+	reportPath := filePath + "/sherlock-scan-report-" + time.String() + "/"
+
+	fmt.Println("Starting firewall scan now!")
+	runCommand("mkdir " + reportPath)
+	runCommand("chmod 777 " + reportPath + "IPv4IptablesRules.txt")
+	fmt.Println("Saving all files to " + reportPath)
+
 	if ipVersion == "ipv4" {
-		runCommand("touch " + filePath + "IPv4IptablesRules.txt")
-		runCommand("iptables -S > " + filePath + "IPv4IptablesRules.txt")
+		runCommand("touch " + reportPath + "IPv4IptablesRules.txt")
+		runCommand("iptables -S > " + reportPath + "IPv4IptablesRules.txt")
 	} else if ipVersion == "ipv6" {
-		runCommand("touch " + filePath + "IPv6IptablesRules.txt")
-		runCommand("ip6tables -S > " + filePath + "IPv6IptablesRules.txt")
+		runCommand("touch " + reportPath + "IPv6IptablesRules.txt")
+		runCommand("ip6tables -S > " + reportPath + "IPv6IptablesRules.txt")
 	}
 }
